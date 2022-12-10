61e870b60ff882cb4a09794251973d28ee19703b
Farhan <animationfar@gmail.com>
2022-12-10 07:30:20 +0700

    feat: add audio.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useId } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/utils/router'
 
+function Audio(){
+  const id = useId();
+
+  document.addEventListener('click', function(){
+    /// @ts-ignore
+    document.getElementById(id).play();
+  });
+
+  return (
+    <audio id={id} src='/Pufino - Dreaming [FTUM Release].mp3' loop={true}></audio>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Audio />
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
