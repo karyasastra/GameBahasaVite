9605f1ee9abfec30c589b0a0ffc8b7eafc9efa5c
Farhan <animationfar@gmail.com>
2022-12-10 07:38:52 +0700

    feat: pengaturan

--- src/components/Home/Menu.tsx
+++ src/components/Home/Menu.tsx
@@ -1,18 +1,24 @@
+import {useState} from 'react';
 import Button from "@/components/Global/Button";
+import PopupPengaturan from "./PopupPengaturan";
 
 export default function Menu(){
+  const [pengaturan, tampilkanPengaturan] = useState(false);
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      position: 'absolute',
-      left: '1.5rem',
-      bottom: '6rem',
-    }}>
-      <Button>Mainkan</Button>
-      <Button style={{marginTop: '1rem'}}>Buku</Button>
-      <Button style={{marginTop: '1rem'}}>Pengaturan</Button>
-      <Button style={{marginTop: '1rem'}}>Keluar</Button>
-    </div>
+    <>
+      {pengaturan && <PopupPengaturan close={ () => tampilkanPengaturan(false) } />}
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'absolute',
+        left: '1.5rem',
+        bottom: '6rem',
+      }}>
+        <Button>Mainkan</Button>
+        <Button style={{marginTop: '1rem'}}>Buku</Button>
+        <Button style={{marginTop: '1rem'}} onClick={()=>tampilkanPengaturan(true)}>Pengaturan</Button>
+        <Button style={{marginTop: '1rem'}}>Keluar</Button>
+      </div>
+    </>
   );
 }
---
+++ src/components/Home/PopupPengaturan.tsx
@@ -0,0 +1,31 @@
+import localforage from 'localforage';
+import { useState, useEffect } from 'react';
+import Button from "../Global/Button";
+import Popup, { TPopupProps } from "../Global/Popup";
+import TextInput from "../Global/TextInput";
+
+export default function PopupPengaturan({ close }:TPopupProps){
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    localforage.getItem('email', function(err, val){
+      setEmail((val as string) ?? 'surelmu@mail.com');
+    });
+  }, []);
+
+  async function storeEmail(){
+    await localforage.setItem('email', email);
+    close && close();
+  }
+
+  return (
+    <Popup close={ close } title='Masukkan Email' withClose={true}>
+      <TextInput type='email' value={ email } onChange={(e) => setEmail(e.target.value)} />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button style={{
+          marginTop: '1rem',
+        }} onClick={ () => (storeEmail()) }>Simpan</Button>
+      </div>
+    </Popup>
+  );
+}
