f04d85db6d0a1eb5fcbd5cb4aa43b2947f64f301
Farhan <animationfar@gmail.com>
2022-12-10 17:16:26 +0700

    fix: audio moved to index.html

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/utils/router'
 
-function Audio(){
-  const id = useId();
-
-  document.addEventListener('click', function(){
-    /// @ts-ignore
-    document.getElementById(id).play();
-  });
-
-  return (
-    <audio id={id} src='/Pufino - Dreaming [FTUM Release].mp3' loop={true} autoPlay={true}></audio>
-  );
-}
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Audio />
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
+/// @ts-ignore
+document.addEventListener('click', function(){
+  /// @ts-ignore
+  document.getElementById('bgm')?.play();
+});
